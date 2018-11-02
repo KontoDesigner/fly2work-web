94178d9afd8d1b9ec5eb81addd3c1de19b113e37
Filip Danielsson <filip.danielsson@tui.se>
2018-11-02 09:27:10 +0100

    hotel start,end mixed

--- src/components/formValidation.js
+++ src/components/formValidation.js
@@ -56,20 +56,20 @@ const formValidation = Yup.object().shape({
     hotelNeeded: Yup.boolean()
         .nullable(true)
         .required('Hotel needed is required'),
-    hotelStart: Yup.string()
+    hotelStart: Yup.mixed()
         .nullable(true)
         .when('hotelNeeded', {
             is: true,
-            then: Yup.string()
+            then: Yup.date()
                 .typeError('Hotel start must be a datetime')
                 .nullable(true)
                 .required('Hotel start is required')
         }),
-    hotelEnd: Yup.string()
+    hotelEnd: Yup.mixed()
         .nullable(true)
         .when('hotelNeeded', {
             is: true,
-            then: Yup.string()
+            then: Yup.date()
                 .typeError('Hotel start must be a datetime')
                 .nullable(true)
                 .required('Hotel end is required')
