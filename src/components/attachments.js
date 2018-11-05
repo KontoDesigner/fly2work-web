789f61ae6686bc6879f56e4600c7e76c5625ddec
Reft <filipdanielsson@hotmail.se>
2018-11-05 17:12:19 +0100

    attachment stuff

--- src/components/attachments.js
+++ src/components/attachments.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { Button } from 'reactstrap'
 import Moment from 'react-moment'
 import * as RestClient from '../infrastructure/restClient'
+import * as ajaxStatusActions from '../actions/ajaxStatusActions'
+import { toastr } from 'react-redux-toastr'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 class Attachments extends Component {
     constructor(props) {
@@ -10,12 +14,50 @@ class Attachments extends Component {
         this.downloadBtn = React.createRef()
     }
 
-    download = attachment => {
-        RestClient.download('pdf', { id: attachment.id }, `${attachment.name}.${attachment.extension}`)
+    download = async attachment => {
+        this.props.ajaxStatusActions.beginAjaxCall()
+
+        const req = {
+            staffId: this.props.staff.id,
+            id: attachment.id
+        }
+
+        await RestClient.download('attachment/download', req, attachment.name)
+
+        this.props.ajaxStatusActions.endAjaxCall()
     }
 
-    upload = file => {
-        RestClient.upload('attachment/upload', file, [{ key: 'staffId', value: this.props.staff.id }])
+    upload = async file => {
+        this.props.ajaxStatusActions.beginAjaxCall()
+
+        const res = await RestClient.upload('attachment/upload', file, [{ key: 'staffId', value: this.props.staff.id }])
+
+        this.props.ajaxStatusActions.endAjaxCall()
+
+        if (res.ok === true) {
+            toastr.success('', 'Successfully uploaded file', res)
+        } else {
+            toastr.error('', 'Could not upload file')
+        }
+    }
+
+    delete = async id => {
+        const req = {
+            staffId: this.props.staff.id,
+            id: id
+        }
+
+        this.props.ajaxStatusActions.beginAjaxCall()
+
+        const res = await RestClient.post('attachment/delete', req)
+
+        this.props.ajaxStatusActions.endAjaxCall()
+
+        if (res.ok === true) {
+            toastr.success('', 'Successfully deleted file', res)
+        } else {
+            toastr.error('', 'Could not delete file')
+        }
     }
 
     render() {
@@ -38,11 +80,11 @@ class Attachments extends Component {
                     <thead>
                         <tr>
                             <th>Name</th>
-                            <th>Extension</th>
                             <th>Created</th>
                             <th>Size</th>
                             <th style={{ width: '1px', padding: '12px 24px 11px 24px' }}>
                                 <Button
+                                    style={{ width: '145px' }}
                                     onClick={() => {
                                         this.downloadBtn.click()
                                     }}
@@ -57,17 +99,21 @@ class Attachments extends Component {
                         {this.props.staff.attachments &&
                             this.props.staff.attachments.map(attachment => (
                                 <tr key={attachment.id}>
-                                    <td className="link">{attachment.name}</td>
+                                    <td onClick={() => this.download(attachment)} className="link">
+                                        {attachment.name}
+                                    </td>
 
-                                    <td className="link">{attachment.extension}</td>
+                                    <td onClick={() => this.download(attachment)} className="link">
+                                        {attachment.created && <Moment format="YYYY-MM-DD mm:HH">{attachment.created}</Moment>}
+                                    </td>
 
-                                    <td className="link">{attachment.created && <Moment format="YYYY-MM-DD mm:HH">{attachment.created}</Moment>}</td>
-
-                                    <td className="link">{attachment.size}</td>
+                                    <td onClick={() => this.download(attachment)} className="link">
+                                        {attachment.size}
+                                    </td>
 
                                     <td>
-                                        <Button onClick={() => this.download(attachment)} className="btn btn-function btn-sm" type="button">
-                                            DOWNLOAD
+                                        <Button onClick={() => this.delete(attachment.id)} className="btn btn-sales btn-sm" type="button">
+                                            DELETE
                                         </Button>
                                     </td>
                                 </tr>
@@ -79,4 +125,13 @@ class Attachments extends Component {
     }
 }
 
-export default Attachments
+function mapDispatchToProps(dispatch) {
+    return {
+        ajaxStatusActions: bindActionCreators(ajaxStatusActions, dispatch)
+    }
+}
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(Attachments)
