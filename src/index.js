5a3d8262db816693f13d5bc96f7ef16563345b9a
Filip Danielsson <filip.danielsson@tui.se>
2018-11-13 12:25:47 +0100

    removed serviceworker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
+// import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
 import store from './infrastructure/store'
 
@@ -12,4 +12,4 @@ ReactDOM.render(
     document.getElementById('root')
 )
 
-serviceWorker.unregister()
+// serviceWorker.unregister()
