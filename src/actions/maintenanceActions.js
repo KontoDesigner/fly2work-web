11f50f0848aa3d9aaf1fb6fb76884dd9397336ae
Filip Danielsson <filip.danielsson@tui.se>
2019-01-08 16:10:54 +0100

    maint

--- src/actions/maintenanceActions.js
+++ src/actions/maintenanceActions.js
@@ -14,7 +14,7 @@ export function getMaintenance() {
         dispatch(beginAjaxCall())
 
         try {
-            const maintenance = await RestClient.get('maintenance')
+            const maintenance = await RestClient.get('maint')
 
             dispatch(getMaintenanceSuccess(maintenance))
         } catch (error) {
