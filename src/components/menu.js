294386d33906615c6079a69a94073acbb5ab4336
Filip Danielsson <filip.danielsson@tui.se>
2019-01-10 10:33:50 +0100

    router activeClassName

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { push as ReactBurgerMenu } from 'react-burger-menu'
-import { Link } from 'react-router-dom'
+import { NavLink as Link } from 'react-router-dom'
 import config from '../infrastructure/config'
 
 const Menu = props => {
@@ -14,27 +14,27 @@ const Menu = props => {
                 outerContainerId={'outer-container'}>
                 <h1>{config.name}</h1>
 
-                <Link className="menu-item" to="/new">
+                <Link activeClassName={'active'} className="menu-item" to="/new">
                     New <span className="text-danger">({props.staffCount.new})</span>
                 </Link>
 
-                <Link className="menu-item" to="/pendinghr">
+                <Link activeClassName={'active'} className="menu-item" to="/pendinghr">
                     Pending HR <span className="text-danger">({props.staffCount.pendingHR})</span>
                 </Link>
 
-                <Link className="menu-item" to="/pendingbtt">
+                <Link activeClassName={'active'} className="menu-item" to="/pendingbtt">
                     Pending BTT <span className="text-danger">({props.staffCount.pendingBTT})</span>
                 </Link>
 
-                <Link className="menu-item" to="/pendingdes">
+                <Link activeClassName={'active'} className="menu-item" to="/pendingdes">
                     Pending DES <span className="text-danger">({props.staffCount.pendingDES})</span>
                 </Link>
 
-                <Link className="menu-item" to="/confirmed">
+                <Link activeClassName={'active'} className="menu-item" to="/confirmed">
                     Confirmed <span className="text-danger">({props.staffCount.confirmed})</span>
                 </Link>
 
-                <Link className="menu-item" to="/overview">
+                <Link activeClassName={'active'} className="menu-item" to="/overview">
                     Overview <span className="text-danger">({props.staffCount.overview})</span>
                 </Link>
 
