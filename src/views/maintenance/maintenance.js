aa6c383609d6c590ac66e5c7f6277ff654862c6f
Filip Danielsson <filip.danielsson@tui.se>
2019-01-23 09:17:53 +0100

    maintenance

--- src/views/maintenance/maintenance.js
+++ src/views/maintenance/maintenance.js
@@ -13,9 +13,12 @@ const styles = {
 const Maintenance = props => {
     return (
         <div>
-            <h2 style={styles.title}>OOPSIE WOOPSIE!!</h2>
+            <h2 style={styles.title}>MAINTENANCE</h2>
 
-            <p>Uwu We made a funky wucky!! A wittle funny boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!</p>
+            <p>
+                Uwu We made a funky wucky!! A wittle funny boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this! (Please
+                check back later)
+            </p>
 
             <div style={styles.imgContainer}>
                 <iframe
